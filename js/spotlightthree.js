ff2bef5466b4cec62566c4a189310fc3f2d0332c
herdofsheep <herdofsheep@users.noreply.github.com>
2021-01-05 11:51:35 +0000

    spotlight working with text

--- js/spotlightthree.js
+++ js/spotlightthree.js
@@ -4,12 +4,17 @@ import { TrackballControls } from 'three/examples/jsm/controls/TrackballControls
 var scene, pickingScene, camera
 var pickingTexture, renderer
 var boxGeo, bgGeo
-var bgMaterial, boxMaterial, textMaterial
+var bgMaterial, boxMaterial, textMaterials
 var backgroundObj, cube
-var controls, text, textGeo;
+var controls, textGeo, textMesh;
 var offset, duplicated, pickingData, cursorType, canClick, link;
 var container, spotLight, lightHelper, shadowCameraHelper;
 var width, height;
+var font;
+
+const   textHeight = 0.5,
+        textSize = 1,
+        textCurveSegments = 4;
 
 init();
 onWindowResize();
@@ -17,12 +22,6 @@ startup();
 animate();
 
 function init(){
-    offset = new THREE.Vector3( 1, 1, 1 );
-    duplicated = false;
-    pickingData = [];
-    cursorType = 'grab';
-    canClick = false;
-    link = ""
 
     scene = new THREE.Scene();
     pickingScene = new THREE.Scene();
@@ -30,31 +29,35 @@ function init(){
     pickingTexture = new THREE.WebGLRenderTarget( 1, 1 );
     renderer = new THREE.WebGLRenderer();
 
-    textMaterial = new THREE.MeshPhongMaterial( { color: 0xFF1493, dithering: true } );
+    //BOXOBJECT
 
     boxGeo = new THREE.BoxGeometry( 1, 1, 1 );
     boxMaterial = new THREE.MeshPhongMaterial( { color: 0xFF1493, dithering: true } );
     cube = new THREE.Mesh( boxGeo, boxMaterial );
 
+    //TEXTMATERIALS
+
+    textMaterials = [
+        new THREE.MeshPhongMaterial( { color: 0xffffff, flatShading: true } ), // front
+        new THREE.MeshPhongMaterial( { color: 0xffffff } ) // side
+    ];
+
+    //BACKGROUND
+
     bgGeo = new THREE.PlaneBufferGeometry( 100 , 100 );
     bgMaterial = new THREE.MeshPhongMaterial( { color: 0x1e1e1e, dithering: true } );
     backgroundObj = new THREE.Mesh( bgGeo, bgMaterial );
 
-    const loader = new THREE.FontLoader();
+    //CONTROLS
 
-    loader.load( 'node_modules/three/examples/fonts/helvetiker_regular.typeface.json', function ( font ) {
-
-        textGeo = new THREE.TextGeometry( 'HELLO', {
-                font: font,
-                size: 1,
-                height: 1,
-                curveSegments: 12,
-            } );
-    
-    } );
-
-    text = new THREE.Mesh( textGeo, textMaterial );
-
+    controls = new TrackballControls( camera, renderer.domElement );
+    controls.rotateSpeed = -1.0;
+    controls.zoomSpeed = 1.2;
+    controls.panSpeed = -0.8;
+    controls.noZoom = false;
+    controls.noPan = false;
+    controls.staticMoving = true;
+    controls.dynamicDampingFactor = 0.3;
     container = document.getElementById("container");
     container.appendChild( renderer.domElement );
 
@@ -77,9 +80,6 @@ function startup() {
     camera.position.z = 5;
     scene.background = new THREE.Color( 0x000000 );
 
-    // const ambient = new THREE.AmbientLight( 0xffffff, 0.1 );
-    // scene.add( ambient );
-
     spotLight = new THREE.SpotLight( 0xffffff, 1 );
     spotLight.position.set( 0, 0, 10 );
     spotLight.angle = Math.PI / 8;
@@ -97,14 +97,7 @@ function startup() {
     scene.add( spotLight );
     scene.add( spotLight.target );
 
-    controls = new TrackballControls( camera, renderer.domElement );
-    controls.rotateSpeed = -1.0;
-    controls.zoomSpeed = 1.2;
-    controls.panSpeed = -0.8;
-    controls.noZoom = false;
-    controls.noPan = false;
-    controls.staticMoving = true;
-    controls.dynamicDampingFactor = 0.3;
+    loadFont();
 
     const ambient = new THREE.AmbientLight( 0xffffff, 0.05 );
     scene.add( ambient );
@@ -135,7 +128,83 @@ function onMouseMove( e ) {
     spotLight.target.position.x = -scale + 2*scale*x/width;
     spotLight.target.position.y = scale -2*scale*y/height;
 
-  }
+}
+
+function loadFont() {
+
+    const loader = new THREE.FontLoader();
+    loader.load( 'node_modules/three/examples/fonts/helvetiker_regular.typeface.json',  function ( response ) {
+        font = response;
+        createText()
+    } );
+
+}
+
+function createText() {
+
+    textGeo = new THREE.TextGeometry( 'ollo', {
+
+        font: font,
+        size: textSize,
+        height: textHeight,
+        curveSegments: textCurveSegments
+
+    } );
+
+    textGeo.computeBoundingBox();
+    textGeo.computeVertexNormals();
+
+    const triangle = new THREE.Triangle();
+
+    // "fix" side normals by removing z-component of normals for side faces
+    // (this doesn't work well for beveled geometry as then we lose nice curvature around z-axis)
+
+    const triangleAreaHeuristics = 0.1 * ( textHeight * textSize );
+
+    for ( let i = 0; i < textGeo.faces.length; i ++ ) {
+
+        const face = textGeo.faces[ i ];
+
+        if ( face.materialIndex == 1 ) {
+
+            for ( let j = 0; j < face.vertexNormals.length; j ++ ) {
+
+                face.vertexNormals[ j ].z = 0;
+                face.vertexNormals[ j ].normalize();
+
+            }
+
+            const va = textGeo.vertices[ face.a ];
+            const vb = textGeo.vertices[ face.b ];
+            const vc = textGeo.vertices[ face.c ];
+
+            const s = triangle.set( va, vb, vc ).getArea();
+
+            if ( s > triangleAreaHeuristics ) {
+
+                for ( let j = 0; j < face.vertexNormals.length; j ++ ) {
+
+                    face.vertexNormals[ j ].copy( face.normal );
+
+                }
+
+            }
+
+        }
+
+    }
+
+    const centerOffset = - 0.5 * ( textGeo.boundingBox.max.x - textGeo.boundingBox.min.x );
+
+    textGeo = new THREE.BufferGeometry().fromGeometry( textGeo );
+
+    textMesh = new THREE.Mesh( textGeo, textMaterials );
+
+    textMesh.position.y = 1;
+
+    scene.add( textMesh );
+
+}
 
 function animate() {
 
